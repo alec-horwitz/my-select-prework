84529f97bcc6d404a5f9442893dbfebdee19a76d
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-01 17:03:15 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  array1 = Array.new
+  while i < array.length
+    temp = yield(array[i])
+    if temp == true
+      array1.puch(temp)
+    end
+    i = i + 1
+  end
+  array1
 end
